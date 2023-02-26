7bb0837ef80deaa5454acebeaf942b4ceaabeb14
Олександр <moskaletzsanych@gmail.com>
2023-02-26 13:19:19 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainer = document.querySelector('#ingredients');
+
+const makeIngredientsList = (obj) => {
+  return obj.map((el) => {
+    const itemEl = document.createElement('li');
+
+    itemEl.textContent = el;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+};
+
+const ingredientsList = makeIngredientsList(ingredients);
+
+ingredientsContainer.append(...ingredientsList);
