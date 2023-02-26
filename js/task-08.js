896d4997d237e88d933f984240f3d63480232acc
Олександр <moskaletzsanych@gmail.com>
2023-02-26 17:18:30 +0200

    tasks-08-09-are-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert(
+      'Ви заповнили не всі поля форми! Будь ласка, заповніть всі поля та повторіть спробу!'
+    );
+  }
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(data);
+
+  event.currentTarget.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorBtn.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textColorEl.textContent = getRandomHexColor();
+});
